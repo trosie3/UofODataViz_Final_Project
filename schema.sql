da405c683864d3c3f4ca20e5ea6e93c9a40e91c9
Anusha GhimireSigdel <85364095+anushaghimire@users.noreply.github.com>
2021-11-10 21:01:39 -0800

    adding schema for cleaned_train

---
+++ schema.sql
@@ -0,0 +1,23 @@
+CREATE TABLE cleaned_train(
+	Type INT,
+	Age INT,
+	Breed1 INT,
+	Breed2 INT,
+	Gender INT,
+	Color1 INT,
+	Color2 INT,
+	Color3 INT,
+	MaturitySize INT,
+	FurLength INT,
+	Vaccinated INT,
+	Dewormed INT,
+	Sterilized INT,
+	Health INT,
+	Quantity INT,
+	VideoAmt INT,
+	PhotoAmt DECIMAL,
+	AdoptionSpeed INT,
+	word_count INT,
+	Fee_bins INT
+	
+	);
