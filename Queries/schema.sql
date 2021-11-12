573f36dba8e98b8b09d811fe072f77363faca014
trosie3 <79772644+trosie3@users.noreply.github.com>
2021-11-11 20:42:11 -0800

    update schema

--- Queries/schema.sql
+++ Queries/schema.sql
@@ -1,4 +1,32 @@
 -- Creating tables for PF_PetDB
+-- right now train table code might have issue cant get csv to upload after creating table so commenting out
+-- CREATE TABLE train(
+	--Type INT,
+	--Name VARCHAR,
+	--Age INT,
+	--Breed1 INT,
+	--Breed2 INT,
+	--Gender INT,
+	--Color1 INT,
+	--Color2 INT,
+	--Color3 INT,
+	--MaturitySize INT,
+	--FurLength INT,
+	--Vaccinated INT,
+	--Dewormed INT,
+	--Sterilized INT,
+	--Health INT,
+	--Quantity INT,
+	--Fee INT,
+	--State INT,
+	--RescuerID VARCHAR,
+	--VideoAmt INT,
+	--Description VARCHAR,
+	--PetID VARCHAR,
+	--PhotoAmt DECIMAL
+	--AdoptionSpeed INT
+	--);
+
 
 CREATE TABLE cleaned_train(
 	Type INT,
@@ -24,7 +52,29 @@ CREATE TABLE cleaned_train(
 	
 	);
 
-	Creating tables for PF_PetDB
+CREATE TABLE cleaned_train_optimized(
+	Type INT,
+	Age INT,
+	Breed1 INT,
+	Breed2 INT,
+	Gender INT,
+	Color1 INT,
+	Color2 INT,
+	Color3 INT,
+	MaturitySize INT,
+	FurLength INT,
+	Vaccinated INT,
+	Dewormed INT,
+	Sterilized INT,
+	Health INT,
+	Quantity INT,
+	VideoAmt INT,
+	PhotoAmt DECIMAL,
+	AdoptionSpeed INT,
+	optimized_word_count INT,
+	Fee_bins INT
+	);
+
 CREATE TABLE breed_labels (
 	breedid INT,
 	type INT,
